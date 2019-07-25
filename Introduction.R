e99c316a09cb60c29e80d054f7e3d585847583da
Wisam Babar <wisambabar@gmail.com>
2019-07-25 16:27:03 +1000

    Subsetting

--- Introduction.R
+++ Introduction.R
@@ -32,3 +32,45 @@ area
 
 round(digits = 2, x = 3.14159)
 
+#vectors
+#c is for combine
+no_members <- c(3,7,10,6)
+length(no_members)
+class(no_members)
+
+respondent_wall_type <- c("muddaub", "Burntbricks", "sunbricks")
+length(respondent_wall_type)
+class(respondent_wall_type)
+str(respondent_wall_type)
+
+possessions <- c("bicycle", "radio", "television")
+possessions <- c(possessions, "mobile_phone") 
+
+#add another element to vector
+possessions <- c("car", possessions)
+
+
+num_char <- c(1,2,3, "a")
+num_logical <- c(1,2,3, FALSE)
+
+#subsetting
+
+respondent_wall_type[2]
+respondent_wall_type[c(3,2)]
+
+more_respondent_wall_type <- respondent_wall_type[c(1,2,3,2,1,3)]
+more_respondent_wall_type
+
+
+no_members[c(TRUE, FALSE, TRUE, TRUE)]
+no_members[no_members > 5]
+no_members[no_members < 3 | no_members > 5]
+no_members[no_members >= 7 & no_members == 3]
+
+possessions[possessions == "car" | possessions == "bicycle"]
+#shortcut
+possessions %in% c("car", "bicycle")
+possessions[possessions %in% c("car", "bicycle")]
+
+
+
