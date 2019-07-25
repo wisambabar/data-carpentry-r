a4d61c2526d4cffb0c7007c3d949c4ca7ca205d2
Wisam Babar <wisambabar@gmail.com>
2019-07-25 12:22:54 +1000

    SAFI clean and SAFI dates

--- scripts/Data_downloads.R
+++ scripts/Data_downloads.R
@@ -3,4 +3,6 @@
 download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_messy.xlsx", destfile = "./data/SAFI_messy.xlsx", mode = "wb")
 
 
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_clean.csv", destfile = "./data/SAFI_clean.csv")
 
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_dates.xlsx", destfile = "./data/SAFI_dates.xlsx", mode = "wb")
