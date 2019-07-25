f207e4531c4107eda38afbd50aa9503812d32a7b
Wisam Babar <wisambabar@gmail.com>
2019-07-25 15:37:22 +1000

    Assign value to area hectares and exercise area

--- Introduction.R
+++ Introduction.R
@@ -3,3 +3,30 @@
 library(tidyverse)
 
 print("Hello, world")
+
+area_hectares <- 1.0
+3+5
+(area_hectares<-1.0)
+
+
+2.47 * area_hectares
+
+(area_hectares <- 2.5)
+
+area_hectares <- 2.5
+area_acres <- 2.47 * area_hectares
+area_acres
+
+area_hectares <- 50
+area_acres <- 2.47 * area_hectares
+area_acres
+
+
+#exercise question
+
+length <- 2.0
+width <- 3.0
+area <- length * width
+area
+
+
